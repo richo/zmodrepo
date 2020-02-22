ecfc8f108af887ccdfc7b7c5cf5a58597aac26bf
Richo Healey <richo@psych0tik.net>
2020-02-21 19:59:08 -0800

    hack at the headers enough to make them build
    vendor zsh's headers to avoid having to figure out how to install them

--- wrapper.h
+++ wrapper.h
@@ -1,6 +1,9 @@
 #ifndef HAVE_STDARG_H
 #define HAVE_STDARG_H
 #endif
+#ifndef HAVE_MEMCPY
+#define HAVE_MEMCPY
+#endif
 #ifndef HAVE_SYS_TYPES_H
 #define HAVE_SYS_TYPES_H
 #endif
@@ -10,5 +13,9 @@
 #ifndef HAVE_TERMIOS_H
 #define HAVE_TERMIOS_H
 #endif
-#include <zsh/zsh_system.h>
-#include <zsh/zsh.h>
+
+#include <sys/time.h>
+#define ZSH_OOT_MODULE
+#define RICHO_HACKS
+#include "./zsh/zsh_system.h"
+#include "./zsh/zsh.h"
